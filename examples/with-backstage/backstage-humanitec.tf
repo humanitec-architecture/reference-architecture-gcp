3c5d17ea1f5d1b6957566fed29b8440606dce456
Johannes Würbach <johannes.wuerbach@googlemail.com>
2024-04-23 14:58:43 +0200

    feat: set backstage service-to-service auth key

--- examples/with-backstage/backstage-humanitec.tf
+++ examples/with-backstage/backstage-humanitec.tf
@@ -77,6 +77,20 @@ resource "humanitec_value" "backstage_cloud_provider" {
   is_secret   = false
 }
 
+resource "random_bytes" "backstage_service_to_service_auth_key" {
+  length = 24
+}
+
+resource "humanitec_value" "app_config_backend_auth_keys" {
+  app_id      = humanitec_application.backstage.id
+  key         = "APP_CONFIG_backend_auth_keys"
+  description = "Backstage service-to-service-auth keys"
+  value = jsonencode([{
+    secret = random_bytes.backstage_service_to_service_auth_key.base64
+  }])
+  is_secret = true
+}
+
 # Configure required resources for backstage
 
 locals {
